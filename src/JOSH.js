4924f16673be1d2143442c195f5800d4c37df826
Andre-PC\Andre <andre.lichtenthaeler@gmail.com>
2018-08-11 11:23:36 +0200

    test

--- src/JOSH.js
+++ src/JOSH.js
@@ -5,72 +5,70 @@
  * @version 1.0.2
  */
 
-var JOSH = (function() {
-  "use strict";
-  
-  var _target, _element, _document = document, _stylesheet = _document.createElement("style"), _josh = "[JOSH]: ";
+var JOSH = (function () {
+	"use strict";
 
-  /**
-   * Appends a css-string to either the head or the body.
-   * @private
-   * @param {string} css 
-   */
-  var _append = function(css) {
-    if (_target === "head") {
-      _element = _document.head;
-    }
-    else if (_target === "body") {
-      _element = _document.body;
-    }
-    else {
-      throw new Error(_josh + "Invalid target: " + _target);
-    }
-    
-    _stylesheet.textContent = css;
-    _element.appendChild(_stylesheet);
-  };
+	var _target, _element, _document = document,
+		_stylesheet = _document.createElement("style"),
+		_josh = "[JOSH]: ";
 
-  return {
-    /** Parses either a string or an object to css.
-     * @public
-     * @since 0.1.0
-     * @param {string|object} args
-     * @param {string} [target]
-     * @returns {!boolean}
-     */
-    parse(args, target = "head") {
-      _target = target.toLowerCase();
+	/**
+	 * Appends a css-string to either the head or the body.
+	 * @private
+	 * @param {string} css 
+	 */
+	var _append = function (css) {
+		if (_target === "head") {
+			_element = _document.head;
+		} else if (_target === "body") {
+			_element = _document.body;
+		} else {
+			throw new Error(_josh + "Invalid target: " + _target);
+		}
 
-      if (!args) {
-        throw new Error(_josh + "No args!");
-      }
-      
-      var result = "";
-      
-      if (typeof args === "object" && args.length >= 1) {
-        for (var selector in args) {
-          result += selector + "{";
-  
-          for (var attribute in args[selector]) {
-            result += attribute + ":" + args[selector][attribute] + ";";
-          }
-  
-          result += "}";
-        }
-        
-        _append(result);
-      }
-      else if (typeof args === "string") {
-        _append(args);
-      }
-      else {
-        throw new Error(_josh + "Wrong type of args: " + typeof args);
-      }
-      return true;
-    }
-  };
+		_stylesheet.textContent = css;
+		_element.appendChild(_stylesheet);
+	};
+
+	return {
+		/** Parses either a string or an object to css.
+		 * @public
+		 * @since 0.1.0
+		 * @param {string|object} args
+		 * @param {string} [target]
+		 * @returns {!boolean}
+		 */
+		parse(args, target = "head") {
+			_target = target.toLowerCase();
+
+			if (!args) {
+				throw new Error(_josh + "No args!");
+			}
+
+			var result = "";
+
+			if (typeof args === "object" && args.length >= 1) {
+				for (var selector in args) {
+					result += selector + "{";
+
+					for (var attribute in args[selector]) {
+						result += attribute + ":" + args[selector][attribute] + ";";
+					}
+
+					result += "}";
+				}
+
+				_append(result);
+			} else if (typeof args === "string") {
+				_append(args);
+			} else {
+				throw new Error(_josh + "Wrong type of args: " + typeof args);
+			}
+			return true;
+		}
+	};
 })();
 
-if(typeof module !== "undefined" && module.exports) {
-  module.exports = JOSH;
+if (typeof module !== "undefined" && module.exports) {
+	module.exports = JOSH;
 }
