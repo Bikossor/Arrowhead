c299fc984cdf360982606b5cf506b240a1243133
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2018-08-12 00:16:01 +0200

    Merge develop into master (#13)
    * Fixed bug #5
    * #6 Rewrite JOSH (#10)
    * #9 Add a manual test
    * #11 Extend the manual test with assets
    * #12 Switch the 'manual test' to a Github Page

--- src/JOSH.js
+++ src/JOSH.js
@@ -5,69 +5,46 @@
  * @version 1.0.2
  */
 
-var JOSH = (function() {
-  "use strict";
-  
-  var _target, _element, _document = document, _stylesheet = _document.createElement("style"), _josh = "[JOSH]: ";
+var JOSH = (function () {
+	"use strict";
 
-  /**
-   * Appends a css-string to either the head or the body.
-   * @private
-   * @param {string} css 
-   */
-  var _append = function(css) {
-    if (_target === "head") {
-      _element = _document.head;
-    }
-    else if (_target === "body") {
-      _element = _document.body;
-    }
-    else {
-      throw new Error(_josh + "Invalid target: " + _target);
-    }
-    
-    _stylesheet.textContent = css;
-    _element.appendChild(_stylesheet);
-  };
+	var styleElement = document.createElement("style"),
+		validHTMLElements = ["head", "body"];
+	
+	var getObjectType = function(obj) {
+		return Object.prototype.toString.call(obj);
+	};
 
-  return {
-    /** Parses either a string or an object to css.
-     * @public
-     * @since 0.1.0
-     * @param {string|object} args
-     * @param {string} [target]
-     * @returns {!boolean}
-     */
-    parse(args, target = "head") {
-      _target = target.toLowerCase();
+	var appendToHTMLElement = function(htmlElement) {
+		if(validHTMLElements.includes(htmlElement)) {
+			document[htmlElement].appendChild(styleElement);
+		}
+		else {
+			throw new Error("HTMLElement '" + htmlElement + "' not valid!");
+		}
+	};
 
-      if (!args) {
-        throw new Error(_josh + "No args!");
-      }
-      
-      var result = "";
-      
-      if (typeof args === "object" && args.length >= 1) {
-        for (var selector in args) {
-          result += selector + "{";
-  
-          for (var attribute in args[selector]) {
-            result += attribute + ":" + args[selector][attribute] + ";";
-          }
-  
-          result += "}";
-        }
-        
-        _append(result);
-      }
-      else if (typeof args === "string") {
-        _append(args);
-      }
-      else {
-        throw new Error(_josh + "Wrong type of args: " + typeof args);
-      }
-      return true;
-    }
-  };
+	return {
+		/** Parses either a string or an object to css.
+		 * @public
+		 * @since 0.1.0
+		 * @param {string|object} args
+		 * @param {string} [target]
+		 * @returns {!boolean}
+		 */
+		parse(args, target) {
+			target = target || "head";
+			appendToHTMLElement(target);
+
+			if(getObjectType(args) === '[object Array]') {
+				for(var key in args) {
+					styleElement.sheet.insertRule(args[key]);
+				}
+			}
+		}
+	};
 })();
-module.exports = JOSH;
+
+if (typeof module !== "undefined" && module.exports) {
+	module.exports = JOSH;
+}
